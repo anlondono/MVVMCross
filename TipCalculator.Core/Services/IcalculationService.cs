77b1576c6207f431c0692618266eba6935d1e04f
Andres Londoño Lopez <andres14l@hotmail.com>
2019-11-22 11:45:18 -0500

    Core Finish

---
+++ TipCalculator.Core/Services/IcalculationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipCalculator.Core.Services
+{
+    public interface IcalculatonService
+    {
+        decimal TipAmount(decimal subTotal, double generosity);
+    }
+}
---
+++ TipCalculator.Core/ViewModels/TipViewModel.cs
@@ -0,0 +1,67 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TipCalculator.Core.Services;
+
+namespace TipCalculator.Core.ViewModels
+{
+    public class TipViewModel : MvxViewModel
+    {
+        private readonly IcalculatonService _calculationService;
+        private decimal _subTotal;
+        private int _generosity;
+        private decimal _tip;
+
+        public TipViewModel(IcalculatonService calculationService)
+        {
+            this._calculationService = calculationService;
+        }
+
+        public decimal SubTotal
+        {
+            get => _subTotal;
+                set
+            {
+                _subTotal = value;
+                RaisePropertyChanged(() => SubTotal);
+                Recalculate();
+            }
+        }
+
+        public decimal Tip
+        {
+            get => _tip;
+            set
+            {
+                _tip = value;
+                RaisePropertyChanged(() => Tip);
+            }
+
+        }
+        public int Generosity
+        {
+            get => _generosity;
+            set
+            {
+                _generosity = value;
+                RaisePropertyChanged(() => _generosity);
+                Recalculate();
+            }
+
+        }
+        public async override Task Initialize()
+        {
+            await base.Initialize();
+
+            SubTotal = 100;
+            Generosity = 10;
+            Recalculate();
+        }
+        private void Recalculate()
+        {
+            Tip = _calculationService.TipAmount(SubTotal, Generosity);
+        }
+    }
+}
